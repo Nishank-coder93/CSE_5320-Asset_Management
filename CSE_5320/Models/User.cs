34dc2cd91182403918500a2ec29ccb6bfde63086
Neil Jones <neil.j.neil@gmail.com>
2018-01-31 22:36:13 -0600

    Updated the user object

--- CSE_5320/Models/User.cs
+++ CSE_5320/Models/User.cs
@@ -6,7 +6,11 @@ using System.Web;
 namespace CSE_5320.Models
 {
     public class User : Base
-    {
+    { 
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
         public int RoleId { get; set; }
     }
 }
