f53a536323e043dc4b1d6311ad01b81454204a09
Neil Jones <neil.j.neil@gmail.com>
2018-02-01 18:38:29 -0600

    Added a map to the dashboard

--- CSE_5320/Models/Home/HomeModel.cs
+++ CSE_5320/Models/Home/HomeModel.cs
@@ -7,6 +7,16 @@ namespace CSE_5320.Models.Home
 {
     public class HomeModel
     {
+        public HomeModel()
+        {
+            Map = new List<Map>();
+        }
+        public List<Map> Map { get; set; }
+    }
 
+    public class Map
+    {
+        public string id { get; set; }
+        public int value { get; set; }
     }
 }
